1d751693afc06dcc09e61210c049d28f2384c198
Matheus Castello <matheus@castello.eng.br>
2018-12-11 00:16:22 -0200

    Targets: RISCV: Main: Add myself in header
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- targets/riscv/main.c
+++ targets/riscv/main.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of Espruino, a JavaScript interpreter for Microcontrollers
  *
- * Copyright (C) 2013 Gordon Williams <gw@pur3.co.uk>
+ * Copyright (C) 2018 Matheus Castello <matheus@castello.eng.br>
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
