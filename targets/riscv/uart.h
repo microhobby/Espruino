b69dfabf417d8fb1575116e3d833677ea42a638a
Matheus Castello <matheus@castello.eng.br>
2018-12-12 16:44:17 -0200

    Targets: Riscv: UART lib
    Use a new UART lib for simplify the use.
    Is based in the work of dgrubb
    https://github.com/dgrubb/HiFive1-UART
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- targets/riscv/uart.h
+++ targets/riscv/uart.h
@@ -9,10 +9,28 @@
 #include <math.h>
 #include "platform.h"
 #include "platform_config.h"
+#include "encoding.h"
+#include "sifive/devices/uart.h"
+#include "irq.h"
 
-extern void _config_hifive_uart(void);
-extern void _putc(char c);
-extern void _puts(const char * s);
-extern int _getc(char * c);
+enum {
+  UART_RXBUF_SIZE = (1 << 3),
+  UART_RXBUF_MASK = (UART_RXBUF_SIZE - 1)
+};
+
+volatile uint8_t rxbuf_head;
+volatile uint8_t rxbuf_tail;
+char rxbuf[UART_RXBUF_SIZE];
+
+void UART_init(unsigned long baud, int stop_bits);
+void UART_deinit();
+void UART_set_baud(unsigned long baud);
+void UART_set_stop_bits(int stop_bits);
+int UART_put_char(char ch, int blocking);
+int UART_get_char(char * ch, int blocking);
+int UART_probe_rx();
+int UART_write(char * msg, int blocking);
+int UART_read_n(char * buffer, int max_chars, char terminator, int blocking);
+void UART_on_rx(function_ptr_t callback);
 
 #endif
