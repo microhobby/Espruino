62ec757e2ec489553e24f12b63c0895b96fd6537
Matheus Castello <matheus@castello.eng.br>
2018-12-12 16:35:36 -0200

    src: jsvar: Remove debug for jsVarSize
    With all freedom-e-sdk linked together all are working well
    jsVarSize are being initialized correctly
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- src/jsvar.c
+++ src/jsvar.c
@@ -268,9 +268,6 @@ void jsvInit(unsigned int size) {
   assert(size==0);
 #endif
 
-  jsVarsSize = JSVAR_CACHE_SIZE;
-  jshDebug(size, jsVarsSize, 0);
-
   jsVarFirstEmpty = jsvInitJsVars(1/*first*/, jsVarsSize);
   jsvSoftInit();
 }
